0606d0351624c8863f57d94089273832f32050d5
topdeveloper0729 <topdev0729@gmail.com>
2021-05-05 09:28:53 -0700

    Generate component product-create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCreateComponent } from './components/product-create/product-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
