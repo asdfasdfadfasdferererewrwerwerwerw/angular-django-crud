edd94c65d2aa5fadbf572ad2472f563fdbb69475
topdeveloper0729 <topdev0729@gmail.com>
2021-05-05 11:28:31 +0900

    feat: created CRUD service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseURL = 'https://localhost:8080/api/products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllProducts(): Observable<any> {
+    return this.httpClient.get(baseURL);
+  }
+  
+  getProducts(id: number): Observable<any> {
+    return this.httpClient.get(`${baseURL}/${id}`);
+  }
+    
+  cteateProducts(data: any): Observable<any> {
+    return this.httpClient.post(baseURL, data);
+  }
+    
+  updateProducts(id: number, data: any): Observable<any> {
+    return this.httpClient.put(`${baseURL}/${id}`, data);
+  }
+    
+  deleteProducts(id: number): Observable<any> {
+    return this.httpClient.delete(`${baseURL}/${id}`);
+  }
+    
+  deleteAllProducts(): Observable<any> {
+    return this.httpClient.delete(baseURL);
+  }
+    
+  getProductsByName(name: any): Observable<any> {
+    return this.httpClient.delete(`${baseURL}?name=${name}`);
+  }
 }
