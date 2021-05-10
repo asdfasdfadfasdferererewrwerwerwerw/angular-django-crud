1b33517183817af2254a956775f3f965a508131f
topdeveloper0729 <topdev0729@gmail.com>
2021-05-10 15:07:50 +0900

    feat: add django as a backend

---
+++ backend/products/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Products(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='')
+    description = models.CharField(max_length=200,blank=False, default='')
+    photo = models.CharField(max_length=200, blank=False, default='')
